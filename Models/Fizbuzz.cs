7c73cf0da803dd9e81fe70b9e928b7904e136066
Siewek <80672357+Siewek@users.noreply.github.com>
2021-03-22 17:43:15 +0100

    Add files via upload

---
+++ Models/Fizbuzz.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FizzBuzzWeb.Models
+{
+    public class Fizbuzz
+    {
+       //[Required, Range(0,1000)]
+        public int Number { get; set; }
+
+        public string FizzBuzz;
+        public void output()
+        {
+            if(Number %3 == 0 && Number %5 == 0)
+            {
+                FizzBuzz = "FizzBuzz";
+            }
+            else if (Number %3 == 0)
+            {
+                FizzBuzz = "Fizz";
+            }
+            else if (Number %5 == 0)
+            {  
+                    FizzBuzz = "Buzz"; 
+            }
+            else
+            {
+                FizzBuzz = "Liczba nie spełnia kryteriów zadania";
+            }
+        }
+
+    }
+}
---
+++ Models/Session.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using FizzBuzzWeb.Models;
+
+namespace FizzBuzzWeb.Models
+{
+    public class Session
+    {
+        public List<Fizbuzz> Session_List = new List<Fizbuzz>();
+    }
+}
